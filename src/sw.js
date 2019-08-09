f3029eb8cd77be67521a4278d20b3412df9bdb0f
Frances Cohen <t-frcohe@microsoft.com>
2019-08-08 20:30:12 -0700

    test

--- src/sw.js
+++ src/sw.js
@@ -1,18 +1,18 @@
-//self.registration.showNotification("hi", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
+self.registration.showNotification("hi", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
 
-self.onnotificationclick = function(event) {
-    console.log('On notification click: ', event.notification.tag);
-    var messageId = event.notification.data;
+// self.onnotificationclick = function(event) {
+//     console.log('On notification click: ', event.notification.tag);
+//     var messageId = event.notification.data;
   
-    event.notification.close();
+//     event.notification.close();
   
-    if (event.action === 'accept-action') {
-      console.log("accept clicked");
-    }
-    else if (event.action === 'reject-action') {
-      console.log("reject clicked")
-    }
-  };
+//     if (event.action === 'accept-action') {
+//       console.log("accept clicked");
+//     }
+//     else if (event.action === 'reject-action') {
+//       console.log("reject clicked")
+//     }
+//   };
 
 //   self.onnotificationclick = function(event) {
 //     console.log('On notification click: ', event.notification.tag);

0f009ceb8e574a627cc760bbc58d013ed9fe3f18
Frances Cohen <t-frcohe@microsoft.com>
2019-08-08 20:22:37 -0700

    test

---
+++ src/sw.js
@@ -0,0 +1,34 @@
+//self.registration.showNotification("hi", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
+
+self.onnotificationclick = function(event) {
+    console.log('On notification click: ', event.notification.tag);
+    var messageId = event.notification.data;
+  
+    event.notification.close();
+  
+    if (event.action === 'accept-action') {
+      console.log("accept clicked");
+    }
+    else if (event.action === 'reject-action') {
+      console.log("reject clicked")
+    }
+  };
+
+//   self.onnotificationclick = function(event) {
+//     console.log('On notification click: ', event.notification.tag);
+//     event.notification.close();
+  
+//     // This looks to see if the current is already open and
+//     // focuses if it is
+//     event.waitUntil(clients.matchAll({
+//       type: "window"
+//     }).then(function(clientList) {
+//       for (var i = 0; i < clientList.length; i++) {
+//         var client = clientList[i];
+//         if (client.url == '/' && 'focus' in client)
+//           return client.focus();
+//       }
+//       if (clients.openWindow)
+//         return clients.openWindow('/');
+//     }));
+//   };

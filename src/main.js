2a61d845bdb30d3bf72d103ebd661f72908e8d2d
Frances Cohen <t-frcohe@microsoft.com>
2019-08-08 20:21:26 -0700

    test

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ window.addEventListener('load', function () {
       }
       if (window.Notification && Notification.permission === "granted") {
         //subscribeUserToPush();
-        registration.showNotification("hi", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
+        //registration.showNotification("hi", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
         //registration.showNotification("hi");
       }
 

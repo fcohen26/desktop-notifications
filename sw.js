99e5faf236e6fd008a1fb4b620993b97c27d2f34
Frances Cohen <t-frcohe@microsoft.com>
2019-08-15 13:16:28 -0700

    test

--- sw.js
+++ sw.js
@@ -25,11 +25,14 @@ self.addEventListener('notificationclick', function(event) {
     console.dir(self);
     console.dir("hi notification clicked");
     console.dir(event.action);
-    event.preventDefault();
     console.log("IS DEFAULT PREVENTED");
     console.log(event.defaultPrevented);
     //clients.openWindow('http://www.facebook.com');
     //notification.close();
+    parent.focus();
+    window.focus();
+    window.open().close();
+    this.close();
     console.log("notification click");
     if (!event.action) {
         let win = event.returnValue;

29ba895107216dfba907404f0cb49c93b65f2ffa
Frances Cohen <t-frcohe@microsoft.com>
2019-08-15 13:19:28 -0700

    test

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ self.addEventListener('notificationclick', function(event) {
     //clients.openWindow('http://www.facebook.com');
     //notification.close();
     parent.focus();
-    //window.focus();
+    window.focus();
     //window.open().close();
     //this.close();
     console.log("notification click");

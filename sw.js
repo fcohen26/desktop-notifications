da7285def903e9ab8cc29e6d251a59bd6792ea7b
Frances Cohen <t-frcohe@microsoft.com>
2019-08-22 15:50:09 -0700

    test

--- sw.js
+++ sw.js
@@ -20,19 +20,12 @@ self.addEventListener('install', function(event) {
 //       console.log("reject clicked")
 //     }
 //   };
-self.addEventListener('notificationclick', function(event) {
-    console.dir("hi notification clicked");
-    console.dir(event.action);
-    console.log("IS DEFAULT PREVENTED");
-    console.log(event.defaultPrevented);
-    //let winR = window.open('http://www.facebook.com');
-   // winR.close();
-    //notification.close();
-    //window.focus();
-    //window.open().close();
-    event.preventDefault();
-    // parent.focus();
 
+// Notification click event listener--triggered when notification is clicked anywhere
+self.addEventListener('notificationclick', function(event) {
+    event.preventDefault();
+
+    // focus tab where notification came  from
     event.waitUntil(clients.matchAll({
         type: "window",
     })
@@ -42,45 +35,25 @@ self.addEventListener('notificationclick', function(event) {
         }
     }))
 
+    // case for general notification click (not on accept or reject)
     if (!event.action) {
-       // let win = event.returnValue;
-       // console.log(win);
-        //win.close();
         console.dir('Notification click');
         return;
     }
+    // case for notification click on accept button (in this case open google search for "accept")
     if (event.action === 'accept-action') {
         console.dir('accept action clicked');
-        alert("Chat accepted");
         // window.focus();
         // this.close();
-        // // clients.openWindow("https://www.facebook.com");
+        clients.openWindow("https://www.google.com/search?q=accept&rlz=1C1CHBF_enUS861US861&oq=accept&aqs=chrome..69i57j0l5.1106j1j4&sourceid=chrome&ie=UTF-8");
         // event.notification.close();
     }
+    // case for notification click on accept button (in this case open google search for "reject")
     if (event.action === 'reject-action') {
         console.dir('reject action clicked');
-        alert("Chat rejectd");
+        window.alert("Chat rejectd");
 
-        // clients.openWindow('https://www.google.com');
+        clients.openWindow('https://www.google.com/search?rlz=1C1CHBF_enUS861US861&ei=zxtfXeLpEMeV0PEPzvCY0A8&q=reject&oq=reject&gs_l=psy-ab.3..0i67i70i249j0i67l2j0l2j0i67j0j0i67j0l2.38718.39413..39615...0.3..0.262.644.4j1j1....2..0....1..gws-wiz.......0i71j0i131.RitfVJR2Wgs&ved=0ahUKEwiijd7wx5fkAhXHCjQIHU44BvoQ4dUDCAo&uact=5');
         event.notification.close();
     }
 });
-
-//   self.onnotificationclick = function(event) {
-//     console.log('On notification click: ', event.notification.tag);
-//     event.notification.close();
-  
-//     // This looks to see if the current is already open and
-//     // focuses if it is
-//     event.waitUntil(clients.matchAll({
-//       type: "window"
-//     }).then(function(clientList) {
-//       for (var i = 0; i < clientList.length; i++) {
-//         var client = clientList[i];
-//         if (client.url == '/' && 'focus' in client)
-//           return client.focus();
-//       }
-//       if (clients.openWindow)
-//         return clients.openWindow('/');
-//     }));
-//   };

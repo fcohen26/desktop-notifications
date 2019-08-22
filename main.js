f4fc3b4b29ac8b81e50808af40472c4691bc009e
Frances Cohen <t-frcohe@microsoft.com>
2019-08-22 16:00:04 -0700

    test

--- main.js
+++ main.js
@@ -1,180 +1,38 @@
 window.addEventListener('load', function () {
+  //register service worker on load and log progress
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      
     if(registration.installing) {
-
       console.log('Service worker installing');
-
     } else if(registration.waiting) {
-
       console.log('Service worker installed');
-
     } else if(registration.active) {
-
       console.log('Service worker active');
+      console.log('service worker scope is:');
       console.log(registration.scope);
-
     }
       console.log("service worker registration succeeded: ", registration);
-
     }, function(error) {
       console.log("service worker registration failed: ", error);
     });
     var button = document.getElementsByTagName('button')[0];
-
+    //event listener on button to trigger notification
     button.addEventListener('click', function () {
-          Notification.requestPermission();
           if (window.Notification && Notification.permission !== "granted") {
-            console.log(Notification.permission);
+            //request permission to show notifications
             Notification.requestPermission(function (status) {
               if (Notification.permission !== status) {
                 Notification.permission = status;
               }
             });
           }
-
-
+          //if notification permission has been granted, send notification 
           if (window.Notification && Notification.permission === "granted") {
-            //service-worker method:
+            //send desktop notification on service worker registration
             navigator.serviceWorker.getRegistration('/sw.js').then(function(registration) {
-              registration.showNotification("hi", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
+              registration.showNotification("Incoming Chat From Visitor X", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
             });
-
-            //non service-worker method:
-            //  let n = new Notification("Chat request from visitor X");
-            // n.onclick = function(event) {
-            //   console.log("here");
-            //   //event.preventDefault(); // prevent the browser from focusing the Notification's tab
-            //   //parent.focus();
-            //   onClick();
-
-            //   window.focus();
-            //   this.close();
-            // }
-      
           }
     });
   }
 });
-function subscribeUserToPush() {
-  return navigator.serviceWorker.register('/sw.js')
-  .then(function(registration) {
-    const subscribeOptions = {
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(
-        'BIYPd4TWeDpWsydk2tWaMVvYRckTuBybktZwRU_AErdvbQBhfd6ESDg2C553K70VJgP1ZR1voR3UEsUlMHAQej8'
-      )
-    };
-
-    return registration.pushManager.subscribe(subscribeOptions);
-  })
-  .then(function(pushSubscription) {
-    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
-    return pushSubscription;
-  }, function(error) {
-    console.log("push subscribion failed", error);
-  });
-}
-
-
-function urlBase64ToUint8Array(base64String) {
-
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-
-  const base64 = (base64String + padding)
-
-    .replace(/\-/g, '+')
-
-    .replace(/_/g, '/');
-
-
-
-  const rawData = window.atob(base64);
-
-  const outputArray = new Uint8Array(rawData.length);
-
-
-
-  for (let i = 0; i < rawData.length; ++i) {
-
-    outputArray[i] = rawData.charCodeAt(i);
-
-  }
-
-  return outputArray;
-
-}
-function onClick() {
-  window.focus();
-}
-  //console.log(Notification.maxActions);
-   // navigator.serviceWorker
-   //      .register('index.js')
-   //      .then(registration => {
-   //          console.log(
-   //              "ServiceWorker registered with scope:",
-   //              registration.scope
-   //          );
-   //      })
-   //      .catch(e => console.error("ServiceWorker failed:", e));
-  // Notification.requestPermission();
-  // //console.log(Notification.permission);
-  // // At first, let's check if we have permission for notification
-  // // If not, let's ask for it
-  // if (window.Notification && Notification.permission !== "granted") {
-  //   console.log(Notification.permission);
-  //   Notification.requestPermission(function (status) {
-  //     if (Notification.permission !== status) {
-  //       Notification.permission = status;
-  //     }
-  //   });
-  // }
-
-//   var button = document.getElementsByTagName('button')[0];
-
-//   button.addEventListener('click', function () {
-//     // If the user agreed to get notified
-//     // Let's try to send ten notifications
-// //     self.registration.showNotification("New mail from Alice", {
-// //   actions: [
-// //     {
-// //       action: 'archive',
-// //       title: 'Archive'
-// //     }
-// //   ]
-// // });
-
-//     // If the user hasn't told if he wants to be notified or not
-//     // Note: because of Chrome, we are not sure the permission property
-//     // is set, therefore it's unsafe to check for the "default" value.
-//     else if (window.Notification && Notification.permission !== "denied") {
-//       Notification.requestPermission(function (status) {
-//         // If the user said okay
-//         if (status === "granted") {
-//           var n = new Notification("Chat Request from Visitor X");
-//           n.onclick = function(event) {
-//             console.log("here");
-//             //event.preventDefault(); // prevent the browser from focusing the Notification's tab
-//             //parent.focus();
-//             window.focus();
-//             this.close();
-//           }
-
-//         }
-
-//         // Otherwise, we can fallback to a regular modal alert
-//         else {
-//           alert("Hi!");
-//         }
-//       });
-//     }
-
-//     // If the user refuses to get notified
-//     else {
-//       // We can fallback to a regular modal alert
-//       alert("Hi!");
-//     }
-//   });
-// });
-
--- sw.js
+++ sw.js
@@ -1,29 +1,7 @@
-self.addEventListener('install', function(event) {
-    console.log('hiiii from service worker');
-    //self.registration.showNotification("Chat request from Visitor X", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
-
-})
-// console.log('hi from service worker');
-
-//self.registration.showNotification("hi", {"actions": [{action: "accept-action", title: "Accept"}, {action: "reject-action", title: "Reject"}]});
-
-// self.onnotificationclick = function(event) {
-//     console.log('On notification click: ', event.notification.tag);
-//     var messageId = event.notification.data;
-  
-//     event.notification.close();
-  
-//     if (event.action === 'accept-action') {
-//       console.log("accept clicked");
-//     }
-//     else if (event.action === 'reject-action') {
-//       console.log("reject clicked")
-//     }
-//   };
-
 // Notification click event listener--triggered when notification is clicked anywhere
 self.addEventListener('notificationclick', function(event) {
     event.preventDefault();
+    event.notification.close();
 
     // focus tab where notification came  from
     event.waitUntil(clients.matchAll({
@@ -43,17 +21,11 @@ self.addEventListener('notificationclick', function(event) {
     // case for notification click on accept button (in this case open google search for "accept")
     if (event.action === 'accept-action') {
         console.dir('accept action clicked');
-        // window.focus();
-        // this.close();
         clients.openWindow("https://www.google.com/search?q=accept&rlz=1C1CHBF_enUS861US861&oq=accept&aqs=chrome..69i57j0l5.1106j1j4&sourceid=chrome&ie=UTF-8");
-        // event.notification.close();
     }
     // case for notification click on accept button (in this case open google search for "reject")
     if (event.action === 'reject-action') {
         console.dir('reject action clicked');
-        window.alert("Chat rejectd");
-
         clients.openWindow('https://www.google.com/search?rlz=1C1CHBF_enUS861US861&ei=zxtfXeLpEMeV0PEPzvCY0A8&q=reject&oq=reject&gs_l=psy-ab.3..0i67i70i249j0i67l2j0l2j0i67j0j0i67j0l2.38718.39413..39615...0.3..0.262.644.4j1j1....2..0....1..gws-wiz.......0i71j0i131.RitfVJR2Wgs&ved=0ahUKEwiijd7wx5fkAhXHCjQIHU44BvoQ4dUDCAo&uact=5');
-        event.notification.close();
     }
 });
